f2bd6f1e1c2bd5c1c065981befe1b57d97102aa5
Zhiting Lin <zlin035@uottawa.ca>
2018-04-17 14:23:18 +0800

    change the title of the electron app

--- src/Root.jsx
+++ src/Root.jsx
@@ -9,7 +9,7 @@ import makeRoutes from './routes'
 
 export default class Root extends React.Component {
   componentWillMount() {
-    document.title = 'Bytom'
+    document.title = 'Bytom Wallet'
   }
 
   render() {
