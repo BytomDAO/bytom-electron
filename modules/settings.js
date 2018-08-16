7336fcc96431ace0f7973b4fa6073516518d29ca
Zhiting Lin <zlin035@uottawa.ca>
2018-08-16 15:11:00 +0800

    add the settings file

---
+++ modules/settings.js
@@ -0,0 +1,63 @@
+const { app } = require('electron')
+const path = require('path')
+
+import logger from './logger'
+
+let instance = null
+
+class Settings {
+  constructor() {
+    if (!instance) {
+      instance = this
+    }
+
+    return instance
+  }
+
+  init() {
+    const logLevel = { logLevel: 'info' }
+    const logFolder = { logFolder: path.join(this.userDataPath, 'logs') }
+    const loggerOptions = Object.assign('info', logLevel, logFolder)
+    logger.setup(loggerOptions)
+  }
+
+  get userDataPath() {
+    return app.getPath('userData')
+  }
+
+  get appDataPath() {
+    // Application Support/
+    return app.getPath('appData')
+  }
+
+  get userHomePath() {
+    return app.getPath('home')
+  }
+
+  get bytomdPath() {
+    return process.env.DEV?
+      path.join(__dirname, '../bytomd/bytomd-darwin_amd64'):
+      glob.sync( path.join(__dirname, '../bytomd/bytomd*'))
+  }
+
+  get bytomdDataPath(){
+    let bytomdDataPath
+    switch (process.platform){
+      case 'win32':
+        bytomdDataPath = `${app.getPath('appData')}/Bytom`
+        break
+      case 'darwin':
+        bytomdDataPath = `${app.getPath('home')}/Library/Bytom`
+        break
+      case 'linux':
+        bytomdDataPath = `${app.getPath('home')}/.bytom`
+    }
+    return bytomdDataPath
+  }
+
+  constructUserDataPath(filePath) {
+    return path.join(this.userDataPath, filePath)
+  }
+}
+
+module.exports = new Settings()
